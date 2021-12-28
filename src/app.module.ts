19e60c34b50e4d33c09538e77acce43542c765a3
chungjung <chungjung.d@gmail.com>
2021-12-28 17:30:24 +0900

    link Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import {DatabaseModule} from "./config/database/database.module";
+import {DatabaseService} from "./config/database/database.service";
+import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({isGlobal:true}),
+      TypeOrmModule.forRootAsync({
+        imports:[DatabaseModule],
+        useClass: DatabaseService,
+        inject: [DatabaseService]
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
